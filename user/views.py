0f0594babb45f0cc94331a2e75dacc4a7ca24b56
melodyww <yangjieuidesigon@163.com>
2018-07-31 12:23:03 +0800

    添加了user-templates

---
+++ user/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def register(request):
+    return None
+
+
+def login(request):
+    return None
+
+
+def logout(request):
+    return None
+
+
+def user_info(request):
+    return None
